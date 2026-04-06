80a0643319016607c6864fd355e1dd2f6affdb31
Victor <victor_hugo_moreira@hotmail.com>
2026-04-06 20:11:31 -0300

    Todos Resolvidos

--- Exercicio09.ConsoleApp/Program.cs
+++ Exercicio09.ConsoleApp/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Qual numero deseja receber o fatorial? ");
+int n = int.Parse(Console.ReadLine());
+int numero = n;
+
+for (int i = n - 1; i > 0; i--)
+{
+    int n1 = n * i;
+    Console.WriteLine($"Fatorial de {numero} X {i} = {n1}");
+    n = n1;
+    numero = n;
+}
+
+Console.ReadKey();
---
+++ Exercicio10.ConsoleApp/Program.cs
@@ -0,0 +1,23 @@
+﻿for (int i = 1; i <= 100; i++)
+{
+    string n = i.ToString();
+
+    if (i % 3 == 0 && i % 5 == 0)
+    {
+        n = "FizzBuzz";
+    }
+    else if (i % 3 == 0)
+    {
+
+        n = "Fizz";
+    }
+    else if (i % 5 == 0)
+    {
+
+        n = "Buzz";
+    }
+
+    Console.WriteLine($"{n} ");
+}
+
+Console.ReadKey();
